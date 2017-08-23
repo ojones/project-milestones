6af048e748e18c37d50276909838491a80df37cc
ojones <wakeupoj@gmail.com>
2017-08-23 01:29:26 -0700

    connect client to feathersjs server and read milestone data

--- project-milestones/src/App.js
+++ project-milestones/src/App.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { socket, feathersClient } from './feathersClient'
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { milestones: [] };
+    const milestones = feathersClient.service('milestones');
+    milestones.find().then( milestonesResonse => {
+      const milestones = milestonesResonse.data;
+      this.setState({milestones});
+      console.log(this.state.milestones)
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,6 +26,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div>
+          There are {this.state.milestones.length} milestones so far :)
+        </div>
       </div>
     );
   }
